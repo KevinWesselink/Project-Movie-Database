b720aa1f4a59246a65b77afe20abe9d2896fc439
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-01-18 14:21:06 +0100

    Basis toegevoegd, met wat includes.

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<?php
+include "../database/db.php";
+include '../src/functions.php';
+?>
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/style.css?=v180120231419">
+    <title>Movie Database</title>
+</head>
+<body>
+
+    <h1>Test</h1>
+
+</body>
+</html>
