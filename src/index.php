4c236316c5bcf27f324ba782e1b15ff44e6daa29
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-01-23 09:10:40 +0100

    Formulier aangemaakt.

--- src/index.php
+++ src/index.php
@@ -17,8 +17,53 @@ include '../src/functions.php';
     <h1>Test</h1>
 
     <form method="post" name="search">
-        <label for=""></label>
+        <input type="text" name="nameTitle" value="" placeholder="Vul naam van acteur of film in" maxlength="255">
+
+        <input type="checkbox" name="genre" value="">
+        <label for="genre">Action</label>
+
+        <input type="checkbox" name="genre" value="">
+        <label for="genre">Adventure</label>
+
+        <input type="checkbox" name="genre" value="">
+        <label for="genre">Animation</label>
+
+        <input type="checkbox" name="genre" value="">
+        <label for="genre">Biography</label>
+
+        <input type="checkbox" name="genre" value="">
+        <label for="genre">Comedy</label>
+
+        <input type="checkbox" name="genre" value="">
+        <label for="genre">Crime</label>
+
+        <input type="checkbox" name="genre" value="">
+        <label for="genre">Drama</label>
+
+        <input type="checkbox" name="genre" value="">
+        <label for="genre">Horror</label>
+
+        <input type="checkbox" name="genre" value="">
+        <label for="genre">Music</label>
+
+        <input type="checkbox" name="genre" value="">
+        <label for="genre">Mystery</label>
+
+        <input type="checkbox" name="genre" value="">
+        <label for="genre">Romance</label>
+
+        <input type="checkbox" name="genre" value="">
+        <label for="genre">Thriller</label>
+
+        <input type="checkbox" name="genre" value="">
+        <label for="genre">War</label>
+
+        <input type="submit" name="submit" value="Zoeken">
     </form>
+
+    <hr>
+
+    <?php //Roep functie op die resultaten toont. ?>
 
 </body>
 </html>
