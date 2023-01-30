e3140acb6aaccc2de507a841c90efdd688aa4b39
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-01-30 11:02:13 +0100

    Input names veranderd.

--- src/index.php
+++ src/index.php
@@ -16,43 +16,43 @@ include '../src/functions.php';
     <form method="post" name="search">
         <input type="text" name="nameTitle" id="nameTitle" value="" placeholder="Vul naam van acteur of film in" maxlength="255">
 
-        <input type="checkbox" name="genre" id="action" value="action">
+        <input type="checkbox" name="action" id="action" value="action">
         <label for="genre">Actie</label>
 
-        <input type="checkbox" name="genre" id="adventure" value="adventure">
+        <input type="checkbox" name="adventure" id="adventure" value="adventure">
         <label for="genre">Avontuur</label>
 
-        <input type="checkbox" name="genre" id="animation" value="animation">
+        <input type="checkbox" name="animation" id="animation" value="animation">
         <label for="genre">Animatie</label>
 
-        <input type="checkbox" name="genre" id="biography" value="biography">
+        <input type="checkbox" name="biography" id="biography" value="biography">
         <label for="genre">Biografie</label>
 
-        <input type="checkbox" name="genre" id="comedy" value="comedy">
+        <input type="checkbox" name="comedy" id="comedy" value="comedy">
         <label for="genre">Komedie</label>
 
-        <input type="checkbox" name="genre" id="crime" value="crime">
+        <input type="checkbox" name="crime" id="crime" value="crime">
         <label for="genre">Misdaad</label>
 
-        <input type="checkbox" name="genre" id="drama" value="drama">
+        <input type="checkbox" name="drama" id="drama" value="drama">
         <label for="genre">Drama</label>
 
-        <input type="checkbox" name="genre" id="horror" value="horror">
+        <input type="checkbox" name="horror" id="horror" value="horror">
         <label for="genre">Horror</label>
 
-        <input type="checkbox" name="genre" id="music" value="music">
+        <input type="checkbox" name="music" id="music" value="music">
         <label for="genre">Muziek</label>
 
-        <input type="checkbox" name="genre" id="mystery" value="mystery">
+        <input type="checkbox" name="mystery" id="mystery" value="mystery">
         <label for="genre">Mysterie</label>
 
-        <input type="checkbox" name="genre" id="romance" value="romance">
+        <input type="checkbox" name="romance" id="romance" value="romance">
         <label for="genre">Romantiek</label>
 
-        <input type="checkbox" name="genre" id="thriller" value="thriller">
+        <input type="checkbox" name="thriller" id="thriller" value="thriller">
         <label for="genre">Thriller</label>
 
-        <input type="checkbox" name="genre" id="war" value="war">
+        <input type="checkbox" name="war" id="war" value="war">
         <label for="genre">Oorlog</label>
 
         <input type="submit" name="submit" value="Zoeken">
